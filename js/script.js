c895e5441d33d367e65aa19d69cbd573775a58cf
daniel281106 <danielgit28@gmail.com>
2023-04-07 21:14:17 -0300

    alteraçao do css e alteraçao do script mais um botao

--- js/script.js
+++ js/script.js
@@ -1,65 +1,119 @@
-const util = {
+const variaveis = {
+    chek: document.querySelector("#chek"),
     teclas: document.querySelectorAll(".bt"),
     opera: document.querySelectorAll(".op"),
-    btlimp: document.querySelector(".li"),
-    btapag: document.querySelector("#apagar"),
-    btigual: document.querySelector("#igual"),
-    result: document.querySelector(".result"),
+    btlimp: document.querySelectorAll(".li"),
+    btapag: document.querySelectorAll("#apagar"),
+    btigual: document.querySelectorAll("#igual"),
+    result: document.querySelector("#resultado"),
     sinal: false,
     decimal: false,
+    calculate: document.querySelectorAll(".calculadora"),
+    teclado: document.querySelectorAll(".teclas"),
+    label: document.querySelectorAll(".label"),
+    ball: document.querySelectorAll(".ball"),
+    result1: document.querySelectorAll(".result"),
+    body: document.querySelectorAll(".body"),
     clean() {
-        this.btlimp.addEventListener('click', (evt) => {
-            this.result.innerHTML = "0"
-            this.sinal = false
-            this.decimal = false
+        this.btlimp.forEach((evt) => {
+            evt.addEventListener('click', () => {
+                this.result.innerHTML = "0"
+                this.sinal = false
+                this.decimal = false
+            })
         })
     },
-    resultar(){
-        
-        this.btigual.addEventListener('click', (evt) => {
-            this.sinal= false
-            this.decimal= false
-            let res = eval(util.result.innerHTML)
-            this.result.innerHTML= res
-        
+    resultar() {
+        this.btigual.forEach((evt) => {
+            evt.addEventListener('click', () => {
+                this.sinal = false
+                this.decimal = false
+                let res = eval(variaveis.result.innerHTML)
+                this.result.innerHTML = res
+
+            })
+        })
+    },
+
+    dark() {
+        this.chek.addEventListener('change', () => {
+            this.body.forEach((evt)=>{
+                evt.classList.toggle("dark")
+            })
+            this.result1.forEach((evt) => {
+                evt.classList.toggle('dark')
+            })
+            this.teclas.forEach((ev) => {
+                ev.classList.toggle('dark')
+            })
+            this.opera.forEach((ev) => {
+                ev.classList.toggle('dark')
+            })
+            this.btapag.forEach((ev) => {
+                ev.classList.toggle('dark')
+            })
+            this.calculate.forEach((ev) => {
+                ev.classList.toggle('dark')
+            })
+            this.btigual.forEach((ev) => {
+                ev.classList.toggle('dark')
+            })
+            this.btlimp.forEach((ev) => {
+                ev.classList.toggle('dark')
+            })
+            this.teclado.forEach((ev) => {
+                ev.classList.toggle('dark')
+            })
+            this.label.forEach((evt) => {
+                evt.classList.toggle('dark')
+            })
+            this.ball.forEach((evt) => {
+                evt.classList.toggle('dark')
+            })
+        })
+    },
+    escrever() {
+        this.teclas.forEach((el) => {
+            el.addEventListener('click', (evt) => {
+                this.sinal = false
+
+                if (evt.target.innerHTML == ",") {
+                    if (!this.decimal) {
+                        this.decimal = true
+                        if (this.result.innerHTML == "0") {
+                            this.result.innerHTML = "0."
+                        } else {
+                            this.result.innerHTML += "."
+                        }
+                    }
+                } else {
+                    if (this.result.innerHTML == "0") {
+                        this.result.innerHTML = ""
+                    }
+                    this.result.innerHTML += evt.target.innerHTML
+                }
+            })
+        })
+    },
+    operar() {
+        this.opera.forEach((el) => {
+            el.addEventListener('click', (evt) => {
+                if (!this.sinal) {
+                    this.sinal = true
+                    this.decimal = false
+                    if (this.result.innerHTML == "0") {
+                        this.result.innerHTML = ""
+                    }
+                    this.result.innerHTML += evt.target.innerHTML
+
+                }
+
+            })
         })
     }
-
 }
-util.teclas.forEach((el) => {
-    el.addEventListener('click', (evt) => {
-        util.sinal = false
-
-        if (evt.target.innerHTML == ",") {
-            if (!util.decimal) {
-                util.decimal = true
-                if (util.result.innerHTML == "0") {
-                    util.result.innerHTML = "0."
-                } else {
-                    util.result.innerHTML += "."
-                }
-            }
-        } else {
-            if (util.result.innerHTML == "0") {
-                util.result.innerHTML = ""
-            }
-            util.result.innerHTML += evt.target.innerHTML
-        }
-    })
-})
-util.opera.forEach((el) => {
-    el.addEventListener('click', (evt) => {
-        if (!util.sinal) {
-            util.sinal = true
-            util.decimal = false
-            if (util.result.innerHTML == "0") {
-                util.result.innerHTML = ""
-            }
-            util.result.innerHTML += evt.target.innerHTML
-
-        }
-
-    })
-})
-util.clean()
-util.resultar()
+variaveis.escrever()
+variaveis.operar()
+variaveis.dark()
+variaveis.clean()
+variaveis.resultar()
